73e7f30f57dd28acd62598f4e7078493b7728765
Akiho Kawada <kawadatakeshiho@gmail.com>
2023-05-14 18:03:40 +0900

    chore: fastchatの中身を更新

--- download_weight.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoModelForCausalLM
-import torch
-
-model = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-13b-delta-v0")
-
-torch.save(model.state_dict(), "/app/weight/vicuna_13b_delta_weights.pth")
