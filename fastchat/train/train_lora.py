a0da81c3888e0c1cc337cd5b3846886324befa89
Akiho Kawada <kawadatakeshiho@gmail.com>
2023-05-15 09:44:28 +0900

    fix:3090だとllamaにはflash attention使えないので、replace_llama_attn_with_flash_attnをoff

--- fastchat/train/train_lora.py
+++ fastchat/train/train_lora.py
@@ -35,11 +35,11 @@ from fastchat.train.train import (
     make_supervised_data_module,
 )
 
-from fastchat.train.llama_flash_attn_monkey_patch import (
-    replace_llama_attn_with_flash_attn,
-)
+# from fastchat.train.llama_flash_attn_monkey_patch import (
+    # replace_llama_attn_with_flash_attn,
+# )
 
-replace_llama_attn_with_flash_attn()
+# replace_llama_attn_with_flash_attn()
 
 
 @dataclass
